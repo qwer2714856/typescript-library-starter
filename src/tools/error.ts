f348fa7be66c24a30fb20fd43869c39f69bd69d7
luchao.ding <116710782@qq.com>
2019-08-15 17:08:02 +0800

    feat: error 实例
    定义一个error 实例和类型 error工厂

---
+++ src/tools/error.ts
@@ -0,0 +1,39 @@
+import { AxiosRequestConfig, AxiosResponse, AxiosError } from '../types'
+
+export class AxiosErrors extends Error {
+  isAxiosError?: boolean
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
+
+  constructor(
+    message: string,
+    config: AxiosRequestConfig,
+    code?: string | null,
+    request?: any,
+    response?: AxiosResponse
+  ) {
+    super(message)
+
+    this.isAxiosError = true
+    this.config = config
+    this.code = code
+    this.request = request
+    this.response = response
+    // 这东西是解决typescript的一个坑，当继承一些内置对象的时候他的原型链子会变为继承对象所以通过这个在改回来。改为它自己。 如果没有这个instanceof 自己就是false
+    Object.setPrototypeOf(this, AxiosErrors.prototype)
+  }
+}
+
+export function factoryError(
+  message: string,
+  config: AxiosRequestConfig,
+  code?: string | null,
+  request?: any,
+  response?: AxiosResponse
+): AxiosError {
+  const AxiosError: AxiosError = new AxiosErrors(message, config, code, request, response)
+
+  return AxiosError
+}
