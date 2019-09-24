7950fe5d9e87bbcd3f9dfe5667d01591578c3044
luchao.ding <116710782@qq.com>
2019-09-24 15:35:30 +0800

    feat: 测试params

--- ex/demo/app.ts
+++ ex/demo/app.ts
@@ -3,4 +3,7 @@ import axios from '../../src/index';
 axios({
     url: '/api',
     method: 'GET',
+    params: {
+        a:[12,23]
+    }
 })
--- src/main/root.ts
+++ src/main/root.ts
@@ -1,9 +1,22 @@
 import { AxiosConfig, MainAxios } from './../types/index'
 import { xhr } from '../core/xhr'
+import { fmtUrl } from '../utils/url'
+import { isObject } from 'util'
 /**
  * 对外输出库的主入口
  */
+function processParams(config: AxiosConfig): string {
+  const { url, params } = config
+  let rs = url
+
+  if (url && isObject(params)) {
+    rs = fmtUrl(params, url)
+  }
+
+  return rs
+}
 const mainAxios: MainAxios = (config: AxiosConfig) => {
+  config.url = processParams(config)
   xhr(config)
 }
 
